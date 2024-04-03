4bbdc14ccf7464e41baf382a99f1bc3f12470e21
Zak <z.abdessmed@univ-batna2.dz>
2024-04-03 02:27:05 +0100

    merging

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Announcements from "../components/Announcements";
 import CardSection from "../components/CardSection";
 import { AdditionalInfo } from "@/components/component/additional-info";
 import { Footer } from "@/components/component/footer";
+import SecondSection from "@/components/SecondSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <SecondSection />
       <AdditionalInfo />
-     { /*<Announcements />*/}
+      {/*<Announcements />*/}
       {/* <CardSection /> */}
       <Footer />
     </>
